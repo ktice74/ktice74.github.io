a1ecfb86b0ddc851b6dcfdf1efc1beb4b4ab29dc
ktice74 <kindy.tice@gmail.com>
2015-05-30 17:00:18 -0400

    work history hover overs

---
+++ js/application.js
@@ -0,0 +1,21 @@
+// $(document).ready(function() {
+//   $(".job, .job-minor").hover(
+//     function() {
+//       $(this).addClass('active-job');
+//     },
+//     function() {
+//       $(this).removeClass('active-job');
+//     }
+//     );
+//   $(".job-minor").hover(
+//     function(){
+//       $(this).next('.job-pic').hide();
+//     });
+//   $(".tmyba").on("click", function(){
+//     $(".job-minor").hide();
+//     $("description").removeClass("hidden");
+//   });
+//   $(".job-minor").on("click", function(){
+//     console.log(this);
+//   })
+// });
