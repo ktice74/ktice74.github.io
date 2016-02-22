70a205f1ce8d59d94351c06feb499df13e8a3544
ktice74 <kindy.tice@gmail.com>
2016-02-22 14:49:25 -0600

    added animation to about and skills section

---
+++ js/waypoints-init.js
@@ -0,0 +1,54 @@
+jQuery(function($) {
+
+( function( window, $, undefined ) {
+  'use strict';
+
+  $('.intro p').waypoint(function() {
+    $(this).toggleClass( 'animated fadeInLeft' );
+  },
+  {
+    offset: '90%'
+    // triggerOnce: true
+  });
+
+
+  $('.intro h2').waypoint(function() {
+    $(this).toggleClass( 'animated fadeInRight' );
+  },
+  {
+    offset: '90%'
+    // triggerOnce: true
+  });
+
+
+  $('#skills .col-md-4').waypoint(function() {
+    $(this).toggleClass( 'animated fadeInUp' );
+  },
+  {
+    offset: '90%'
+    // triggerOnce: true
+  });
+
+
+  $('.title').waypoint(function() {
+    $(this).toggleClass( 'animated zoomIn' );
+  },
+  {
+    offset: '90%'
+    // triggerOnce: true
+  });
+
+  $('.roll-wrapper .prod_3').waypoint(function() {
+    $(this).toggleClass( 'animated fadeInRight' );
+  },
+  {
+    offset: '100%'
+    // triggerOnce: true
+  });
+
+
+
+
+})( this, jQuery );
+
+});
